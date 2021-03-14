1c1ab6de9e9583958469cb17bf0b0594d37a455e
Riyaz-creator <78251933+Riyaz-creator@users.noreply.github.com>
2021-03-14 13:12:13 +0530

    Update fabo.py

--- fabo.py
+++ fabo.py
@@ -4,7 +4,7 @@ nterms = int(input("How many terms? "))
 # first two terms
 n1, n2 = 0, 1
 count = 0
-
+# new
 # check if the number of terms is valid
 if nterms <= 0:
    print("Please enter a positive integer")
